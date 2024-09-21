79ac363d5dca8242251227ab94c3c4073f2e578c
Dinh Manh <tranmanhproh1@gmail.com>
2024-09-21 10:27:57 +0700

    feat: [PBL6-32] Configure CI CD

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -52,6 +52,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 builder.Services.AddAuthorization();
 
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 var app = builder.Build();
 
 var dbInit = app.Services.GetRequiredService<IDbInitializer>();
@@ -62,14 +64,17 @@ if (app.Environment.IsProduction())
 
 await dbInit.Initialize();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseMiddleware<CustomUnauthorizedMiddleware>();
-app.UseHttpsRedirection();
+if (app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseRouting();
 
