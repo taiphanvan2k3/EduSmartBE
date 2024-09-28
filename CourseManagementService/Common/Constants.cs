63285bc37efef6f5a5f3b86c58c02d0aec94a098
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-09-28 20:16:51 +0700

    fix: [PBL6-69] Fix connection string of services

--- CourseManagementService/Common/Constants.cs
+++ CourseManagementService/Common/Constants.cs
@@ -8,6 +8,6 @@ namespace CourseManagementService.Common
 
         public static readonly string SERVICE_NAME = (typeof(Constants).Namespace ?? "CourseManagementService").Split('.')[0];
 
-        public static readonly string CONNECTION_STRING = "Host=edu-smart.postgres.database.azure.com;Database=EduSmart.AuthService;Username=pbl6duter;Password=Edusmartk21;SSL Mode=Require;Trust Server Certificate=true";
+        public static readonly string CONNECTION_STRING = "Host=edu-smart.postgres.database.azure.com;Database=EduSmart.CourseManagementService;Username=pbl6duter;Password=Edusmartk21;SSL Mode=Require;Trust Server Certificate=true";
     }
 }
