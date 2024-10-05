2d59bec5551bac98d59f42ccc550a1c0c4ebb237
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-10-05 20:39:19 +0700

    feat: [PBL6-85] Write logic to set authorize automatically

--- AuthService/wwwroot/swagger/custom-swagger.js
+++ AuthService/wwwroot/swagger/custom-swagger.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     let eventClickId = "";
+    let isExecutedReplaceBr = false;
+    let isExecutedSetAuth = false;
 
     const replaceBr = function (description) {
         const formattedHtml = description.innerHTML
@@ -9,25 +11,118 @@ document.addEventListener("DOMContentLoaded", function () {
             .replace(/<br \/>Created by/g, "- Created by"); // Xoá <br/> trước "Created by"
 
         description.innerHTML = formattedHtml;
+        isExecutedReplaceBr = true;
     };
 
-    const updateDescriptions = function () {
-        const descriptions = document.querySelectorAll(
-            ".opblock-summary-description"
+    const updateDescriptions = function (descriptionElements) {
+        descriptionElements.forEach(replaceBr);
+    };
+
+    const setAuth = function () {
+        const swaggerToken = localStorage.getItem("swagger_access_token");
+        const unlockButton = document.querySelector(
+            "button.authorize.unlocked"
         );
 
-        descriptions.forEach(replaceBr);
+        if (swaggerToken && unlockButton) {
+            unlockButton.click();
+
+            const bearerValueElement =
+                document.getElementById("auth-bearer-value");
+            if (bearerValueElement) {
+                const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+                    window.HTMLInputElement.prototype,
+                    "value"
+                ).set;
+
+                nativeInputValueSetter.call(bearerValueElement, swaggerToken);
+                bearerValueElement.dispatchEvent(
+                    new Event("change", { bubbles: true })
+                );
+
+                document
+                    .querySelector(".auth-container button.authorize")
+                    .click();
+                document
+                    .querySelector(".auth-container button.btn-done")
+                    .click();
+
+                isExecutedSetAuth = true;
+            }
+        }
     };
 
-    setTimeout(function () {
+    const handleAuthClick = function () {
+        const accessToken = document.querySelector("#auth-bearer-value").value;
+        localStorage.setItem("swagger_access_token", accessToken);
+    };
+
+    const saveAccessToken = function () {
+        const mutationObserver = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                if (mutation.addedNodes.length) {
+                    const authButton = document.querySelector(
+                        ".auth-container button.authorize"
+                    );
+
+                    if (authButton) {
+                        authButton.addEventListener("click", handleAuthClick, {
+                            once: true,
+                        });
+                    }
+
+                    const logoutButton = document.querySelector(
+                        `.button.auth[aria-label="Remove authorization"]`
+                    );
+
+                    if (logoutButton) {
+                        logoutButton.addEventListener("click", function () {
+                            localStorage.removeItem("swagger_access_token");
+                        });
+                    }
+                }
+            });
+        });
+
+        mutationObserver.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    };
+
+    let replaceBrId = setInterval(function () {
         // Cập nhật mô tả khi nhấn vào các thẻ
         document.querySelectorAll(".opblock-tag").forEach(function (tag) {
             tag.addEventListener("click", function () {
                 clearTimeout(eventClickId);
-                eventClickId = setTimeout(updateDescriptions, 200);
+                eventClickId = setTimeout(function () {
+                    // Phần tử tiếp theo của tag
+                    const nextElement = tag.nextElementSibling;
+                    nextElement &&
+                        updateDescriptions(
+                            nextElement.querySelectorAll(
+                                ".opblock-summary-description"
+                            )
+                        );
+                }, 500);
             });
         });
 
-        updateDescriptions();
-    }, 500);
+        updateDescriptions(
+            document.querySelectorAll(".opblock-summary-description")
+        );
+
+        if (isExecutedReplaceBr) {
+            clearInterval(replaceBrId);
+        }
+    }, 100);
+
+    let setAuthId = setInterval(function () {
+        setAuth();
+        if (isExecutedSetAuth) {
+            clearInterval(setAuthId);
+        }
+    }, 100);
+
+    saveAccessToken();
 });
