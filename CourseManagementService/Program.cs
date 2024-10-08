6090b1d7ef16639e2c18ba237c2cf3e93d509d7b
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-10-08 10:08:49 +0700

    fix: [PBL6-91] Add localhost to specificOrigins

--- CourseManagementService/Program.cs
+++ CourseManagementService/Program.cs
@@ -63,15 +63,6 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseCors("AllowAllOrigins");
-}
-else
-{
-    // TODO: Change to AllowSpecificOrigin when finish frontend
-    app.UseCors("AllowAllOrigins");
-}
-
+app.UseCors("AllowSpecificOrigin");
 app.MapControllers();
 await app.RunAsync();
