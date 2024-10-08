b10da65b1fe456bf58373b3d62d9624f54788708
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-10-08 12:34:19 +0700

    feat: [PBL6-92] API for forgot password

---
+++ AuthService/Services/Auth/Schemas/ForgotPasswordContent.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Services.Auth.Schemas
+{
+    public class ForgotPasswordContent
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+    }
+}
