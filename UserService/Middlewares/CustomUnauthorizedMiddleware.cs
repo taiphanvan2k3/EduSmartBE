f94ededcfe3944c79bde5ad710a05f884f3e2858
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-11-09 13:48:01 +0700

    fix: [PBL6-184] Sync naming rules for all services

--- UserService/Middlewares/CustomUnauthorizedMiddleware.cs
+++ UserService/Middlewares/CustomUnauthorizedMiddleware.cs
@@ -17,6 +17,11 @@ namespace UserService.Middlewares
                 return;
             }
 
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
             // Sau khi xử lý request, kiểm tra response status code
             if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
             {
@@ -27,7 +32,7 @@ namespace UserService.Middlewares
                     StatusCode = 401,
                     Message = "You need to be authenticated to access this resource.",
                 };
-                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response, options));
             }
             else if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
             {
@@ -38,7 +43,7 @@ namespace UserService.Middlewares
                     Error = "Forbidden",
                     Message = "You are not authorized to access this resource.",
                 };
-                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response, options));
             }
         }
     }
