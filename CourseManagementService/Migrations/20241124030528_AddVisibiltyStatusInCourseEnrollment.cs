f6597b8b5d7d1f22a0d657900d70bda66e43594c
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-11-24 10:56:51 +0700

    feat: [PBL6-136] Implement API for private/public courses of teacher, student

---
+++ CourseManagementService/Migrations/20241124030528_AddVisibiltyStatusInCourseEnrollment.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseManagementService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVisibiltyStatusInCourseEnrollment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "LessonTrackings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "CourseEnrollments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VisibilityStatus",
+                table: "CourseEnrollments",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Unknown",
+                comment: "Allow other students to see the progress of this student in this course");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseEnrollments_CourseId_StudentId_LeaveDate",
+                table: "CourseEnrollments",
+                columns: new[] { "CourseId", "StudentId", "LeaveDate" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseEnrollments_StudentId_LeaveDate",
+                table: "CourseEnrollments",
+                columns: new[] { "StudentId", "LeaveDate" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CourseEnrollments_CourseId_StudentId_LeaveDate",
+                table: "CourseEnrollments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CourseEnrollments_StudentId_LeaveDate",
+                table: "CourseEnrollments");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "LessonTrackings");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "CourseEnrollments");
+
+            migrationBuilder.DropColumn(
+                name: "VisibilityStatus",
+                table: "CourseEnrollments");
+        }
+    }
+}
--- CourseManagementService/Services/CourseManagement/Student/ListOfStudentCoursesService.cs
+++ CourseManagementService/Services/CourseManagement/Student/ListOfStudentCoursesService.cs
@@ -1,4 +1,8 @@
-using CourseManagementService.Services.CourseManagement.Public.Schemas;
+using CourseManagementService.Common;
+using CourseManagementService.Common.Helpers;
+using CourseManagementService.Enumerations;
+using CourseManagementService.Services.Cache;
+using CourseManagementService.Services.CourseManagement.Student.Schemas;
 using CourseManagementService.Services.CourseManagement.Teacher.Schemas;
 using CourseManagementService.Services.Grpc;
 using Microsoft.EntityFrameworkCore;
@@ -7,64 +11,198 @@ namespace CourseManagementService.Services.CourseManagement.Student
 {
     public interface IListOfStudentCoursesService
     {
-        public Task<List<CourseDetailWithTeacherDto>> GetEnrolledCourses();
+        /// <summary>
+        /// Get all courses that student enrolled
+        /// <para>Created at: 2024/10/19</para>
+        /// <para>Created by: TaiPV</para>
+        /// </summary>
+        public Task<ResponseInfo> GetEnrolledCourses();
+
+        /// <summary>
+        /// View course progress of one student
+        /// <para>Created at: 2024/11/23</para>
+        /// <para>Created by: TaiPV</para>
+        /// </summary>
+        /// <param name="userId">Id of student to view</param>
+        /// <param name="isGetAll">True to get all courses, false to get only public courses</param>
+        /// <returns></returns>
+        public Task<ResponseInfo> GetCoursesByUserId(int userId, bool isGetAll = false);
+
+
+        /// <summary>
+        /// Update the visibility status of all courses (private/friends/public)
+        /// <para>Created at: 2024/11/23</para>
+        /// <para>Created by: TaiPV</para>
+        /// </summary>
+        /// <returns></returns>
+        public Task<ResponseInfo> UpdateAllCourseVisibilityStatus(UpdateAllVisibilityStatus request);
     }
 
     public class ListOfStudentCoursesService(IServiceProvider serviceProvider, ILogger<ListOfStudentCoursesService> logger)
         : BaseService(serviceProvider, logger), IListOfStudentCoursesService
     {
-        private readonly string _serviceName = nameof(ListOfStudentCoursesService);
         private readonly IGrpcUserService _grpcUserService = serviceProvider.GetService<IGrpcUserService>()
             ?? throw new ArgumentNullException(ServiceInjectionError("IGrpcUserService"));
+        private readonly ICacheService _cacheService = serviceProvider.GetService<ICacheService>()
+            ?? throw new ArgumentNullException(ServiceInjectionError("ICacheService"));
 
-        public async Task<List<CourseDetailWithTeacherDto>> GetEnrolledCourses()
+        public async Task<ResponseInfo> GetEnrolledCourses()
         {
             var method = GetActualAsyncMethodName();
             try
             {
-                _logger.LogInformation("[{ServiceName}] {MethodName} Start", _serviceName, method);
+                LogInfo("Start", method);
 
+                var currentUser = GetCurrentUser();
+                var responseInfo = await GetCoursesByUserId(currentUser.UserId, isGetAll: true);
+
+                LogInfo("End", method);
+                return responseInfo;
+            }
+            catch (Exception e)
+            {
+                LogError(e, method);
+                throw;
+            }
+        }
+
+        public async Task<ResponseInfo> GetCoursesByUserId(int userId, bool isGetAll = false)
+        {
+            var method = GetActualAsyncMethodName();
+            try
+            {
+                LogInfo("Start", method);
+                var responseInfo = new ResponseInfo();
+
+                if (!await _grpcUserService.CheckUserExist(userId))
+                {
+                    return CreateEarlyResponseInfo(StatusCodes.Status404NotFound, "Student not found");
+                }
+
+                var currentUser = GetCurrentUser();
+                isGetAll = isGetAll || currentUser.UserId == userId;
+
+                var cacheKey = isGetAll
+                    ? CacheManager.EnrolledCourses.Key(userId)
+                    : CacheManager.CourseProgressOfOtherUser.Key(userId);
+
+                var cachedData = _cacheService.GetData<List<EnrolledCourseInfo>>(cacheKey);
+
+                if (cachedData != null)
+                {
+                    responseInfo.Data.Add("courses", cachedData);
+                    return responseInfo;
+                }
+
+                var visibilityEnumType = typeof(CourseProgressVisibility);
                 var courses = await _context.CourseEnrollments
-                    .Where(x => x.StudentId == _appStateService.UserInfo.UserId)
-                    .OrderByDescending(x => x.EnrollmentDate)
-                    .Select(c => new CourseDetailWithTeacherDto()
+                    .Where(x => x.StudentId == userId && (isGetAll || x.VisibilityStatus == CourseProgressVisibility.Public))
+                    .Select(x => new EnrolledCourseInfo()
                     {
-                        Id = c.Course.Id,
-                        Name = c.Course.Name,
-                        Description = c.Course.Description,
-                        CoreValues = c.Course.CoreValues,
-                        Prerequisites = c.Course.Prerequisites,
-                        ThumbnailURL = c.Course.ThumbnailURL,
-                        TotalStudents = c.Course.Enrollments.Count,
+                        Id = x.CourseId,
+                        Name = x.Course.Name,
+                        ThumbnailURL = x.Course.ThumbnailURL,
+                        Description = x.Course.Description,
+                        TotalStudents = x.Course.Enrollments.Count,
+                        VisibilityStatus = new LookupDto()
+                        {
+                            Id = ((int)Enum.Parse(visibilityEnumType, x.VisibilityStatus.ToString())).ToString(),
+                            Name = Utils.GetEnumName(x.VisibilityStatus)
+                        },
+                        TotalLessons = x.Course.Chapters.Sum(ch => ch.Lessons.Count),
                         Teacher = new TeacherDetail()
                         {
-                            Id = c.Course.TeacherId
+                            Id = x.Course.TeacherId
                         }
                     })
                     .ToListAsync();
 
-                var studentIds = courses.Select(x => x.Teacher.Id).Distinct().ToList();
-                var teachersDict = courses.GroupBy(x => x.Teacher.Id)
-                    .ToDictionary(g => g.Key, g => g.First().Teacher);
-
-                // Get teachers' info from UserService
-                var teachers = await _grpcUserService.GetListOfTeachers(studentIds);
-                foreach (var teacher in teachers.Users)
+                if (courses.Count > 0)
                 {
-                    if (teachersDict.TryGetValue(teacher.Id, out var teacherDetail))
+                    await FillTeachersInfo(courses);
+
+                    var courseIds = courses.Select(x => x.Id).ToList();
+                    var courseIdsDict = courses.ToDictionary(x => x.Id, x => x);
+
+                    var lessonTrackings = await _context.LessonTrackings
+                        .Where(x => x.StudentId == userId && courseIds.Contains(x.CourseId))
+                        .GroupBy(x => x.CourseId)
+                        .Select(x => new
+                        {
+                            CourseId = x.Key,
+                            CompletedLessonCount = x.Count(l => l.IsCompleted),
+                            TimeSpent = x.Sum(x => x.TimeSpent)
+                        })
+                        .ToListAsync();
+
+                    foreach (var lessonTracking in lessonTrackings)
                     {
-                        teacherDetail.FullName = teacher.FullName;
-                        teacherDetail.AvatarURL = teacher.AvatarURL;
+                        if (courseIdsDict.TryGetValue(lessonTracking.CourseId, out var course))
+                        {
+                            course.CompletedLessons = lessonTracking.CompletedLessonCount;
+                            course.TimeSpent = lessonTracking.TimeSpent;
+                        }
                     }
                 }
 
-                _logger.LogInformation("[{ServiceName}] {MethodName} End", _serviceName, method);
-                return courses;
+                _cacheService.SetData(cacheKey, courses, DateTimeOffset.Now.AddMinutes(
+                    CacheManager.CourseProgressOfOtherUser.ExpireTimeInMinutes));
+
+                LogInfo("End", method);
+                responseInfo.Data.Add("courses", courses);
+                return responseInfo;
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "[{ServiceName}] {MethodName} Error", _serviceName, method);
+                LogError(e, method);
                 throw;
+            }
+        }
+
+        public async Task<ResponseInfo> UpdateAllCourseVisibilityStatus(UpdateAllVisibilityStatus request)
+        {
+            var method = GetActualAsyncMethodName();
+            try
+            {
+                LogInfo("Start", method);
+                var responseInfo = new ResponseInfo();
+
+                var currentUser = GetCurrentUser();
+                await _context.CourseEnrollments
+                    .Where(c => c.StudentId == currentUser.UserId)
+                    .ExecuteUpdateAsync(c => c.SetProperty(x => x.VisibilityStatus, request.VisibilityStatus));
+
+                _cacheService.RemoveData(CacheManager.CourseProgressOfOtherUser.Key(currentUser.UserId));
+                _cacheService.RemoveData(CacheManager.EnrolledCourses.Key(currentUser.UserId));
+
+                responseInfo.Data.Add("currentStatus", Utils.GetEnumName(request.VisibilityStatus));
+
+                LogInfo("End", method);
+                return responseInfo;
+            }
+            catch (Exception e)
+            {
+                LogError(e, method);
+                throw;
+            }
+        }
+
+        private async Task FillTeachersInfo(List<EnrolledCourseInfo> courses)
+        {
+            var teacherIds = courses.Select(x => x.Teacher.Id).Distinct().ToList();
+
+            var teachersResponse = await _grpcUserService.GetListOfTeachers(teacherIds);
+            Dictionary<int, GrpcServices.SimpleUserResponse> teachersDict = teachersResponse.Users
+                .ToDictionary(x => x.Id, x => x);
+
+            foreach (var course in courses)
+            {
+                if (teachersDict.TryGetValue(course.Teacher.Id, out var teacherInfo))
+                {
+                    course.Teacher.FullName = teacherInfo.FullName;
+                    course.Teacher.Email = teacherInfo.Email;
+                    course.Teacher.AvatarURL = teacherInfo.AvatarURL;
+                }
             }
         }
     }
