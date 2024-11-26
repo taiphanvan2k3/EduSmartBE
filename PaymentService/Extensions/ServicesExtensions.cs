863b4e5278e35d1140266413f3403eb4aec4b683
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-11-26 09:27:15 +0700

    chore: [PBL6-226] Setup SignalR

--- PaymentService/Extensions/ServicesExtensions.cs
+++ PaymentService/Extensions/ServicesExtensions.cs
@@ -64,6 +64,22 @@ namespace PaymentService.Extensions
                     ValidateAudience = true,
                     ValidAudience = configuration["JWTSetting:Audience"],
                 };
+
+                // Enable JWT for SignalR
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
             });
 
             return services;
