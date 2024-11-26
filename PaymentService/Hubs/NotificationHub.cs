27bdeb03cc9620e3ea99e74986f74834eb33fc88
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-11-26 20:12:46 +0700

    feat: [PBl6-226] Apply SignalR to code

--- PaymentService/Hubs/NotificationHub.cs
+++ PaymentService/Hubs/NotificationHub.cs
@@ -6,5 +6,32 @@ namespace PaymentService.Hubs
     {
         public static readonly string CONNECTED = "Connected";
         public static readonly string DISCONNECTED = "Disconnected";
+
+        public override async Task OnConnectedAsync()
+        {
+            var connectionId = Context.ConnectionId;
+            Console.WriteLine($"Client connected: {connectionId}");
+
+            await base.OnConnectedAsync();
+
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                var userIdentifier = Context.UserIdentifier;
+                await Clients.Caller.SendAsync(CONNECTED, connectionId, $"Connected to the server as {userIdentifier}");
+            }
+            else
+            {
+                await Clients.Caller.SendAsync(CONNECTED, connectionId, "Connected to the server");
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var connectionId = Context.ConnectionId;
+            Console.WriteLine($"Client disconnected: {connectionId}");
+
+            await base.OnDisconnectedAsync(exception);
+            await Clients.Caller.SendAsync(DISCONNECTED, connectionId, "Disconnected from the server");
+        }
     }
 }
