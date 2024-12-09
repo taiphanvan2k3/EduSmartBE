a5237529e75b65aacf71d7067fe23e68d1e3af0f
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-12-09 12:57:37 +0700

    chore: [PBL6-249] Modify the UploadHistory table

--- PaymentService/Databases/Schemas/UploadHistory.cs
+++ PaymentService/Databases/Schemas/UploadHistory.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using PaymentService.Enumerations;
 
 namespace PaymentService.Databases.Schemas
@@ -8,7 +10,28 @@ namespace PaymentService.Databases.Schemas
 
         public long StorageAmount { get; set; }
 
-        public DateTimeOffset CreatedAt { get; set; }
+        /// <summary>
+        /// The resource type that the file is uploaded for.
+        /// </summary>
+        [Comment("The resource type that the file is uploaded for.")]
+        public ResourceType ResourceType { get; set; }
+
+        /// <summary>
+        /// Id of the resource that the file is uploaded for. <br/>
+        /// Such as CourseId, VideoLessonId, AttachmentId, CommentId...
+        /// </summary>
+        [Comment("CourseId, VideoLessonId, AttachmentId, CommentId...")]
+        public Guid ResourceId { get; set; }
+
+        public DateTimeOffset UploadDate { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string FileName { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string FilePath { get; set; }
 
         public FileType FileType { get; set; }
 
---
+++ PaymentService/Migrations/20241209044203_ModifyUploadHistory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyUploadHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "UploadHistories",
+                newName: "UploadDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "UploadHistories",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "UploadHistories",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ResourceId",
+                table: "UploadHistories",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                comment: "CourseId, VideoLessonId, AttachmentId, CommentId...");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResourceType",
+                table: "UploadHistories",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                comment: "The resource type that the file is uploaded for.");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UploadHistories_ResourceType_ResourceId",
+                table: "UploadHistories",
+                columns: new[] { "ResourceType", "ResourceId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UploadHistories_ResourceType_ResourceId",
+                table: "UploadHistories");
+
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "UploadHistories");
+
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "UploadHistories");
+
+            migrationBuilder.DropColumn(
+                name: "ResourceId",
+                table: "UploadHistories");
+
+            migrationBuilder.DropColumn(
+                name: "ResourceType",
+                table: "UploadHistories");
+
+            migrationBuilder.RenameColumn(
+                name: "UploadDate",
+                table: "UploadHistories",
+                newName: "CreatedAt");
+        }
+    }
+}
