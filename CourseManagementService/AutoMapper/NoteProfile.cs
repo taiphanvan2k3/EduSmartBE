e33de97c69aa5083b925aae7882c103bf69b880e
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-12-19 18:02:18 +0700

    fix: [PBL6-277] Add more properties to the list of my notes api

--- CourseManagementService/AutoMapper/NoteProfile.cs
+++ CourseManagementService/AutoMapper/NoteProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using CourseManagementService.Services.ChapterManagement.Schemas;
 using CourseManagementService.Services.NoteManagement.Schemas;
 using TblNote = CourseManagementService.Database.Schemas.Note;
 
@@ -9,7 +10,13 @@ namespace CourseManagementService.AutoMapper
         public NoteProfile()
         {
             CreateMap<NoteCreateDto, TblNote>();
-            CreateMap<TblNote, NoteDetail>();
+            CreateMap<TblNote, NoteDetail>()
+                .ForMember(d => d.LessonName, opt => opt.MapFrom(s => s.Lesson.Title))
+                .ForMember(d => d.ChapterInfo, opt => opt.MapFrom(s => new SimpleChapterInfo()
+                {
+                    Order = s.Chapter.Order,
+                    Name = s.Chapter.Name
+                }));
         }
     }
 }
