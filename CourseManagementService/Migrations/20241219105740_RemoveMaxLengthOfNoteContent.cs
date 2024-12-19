7f83be6c6dd3dc4eccb78eaadc4f44e0f36cc069
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-12-19 18:02:18 +0700

    fix: [PBL6-277] Add more properties to the list of my notes api

---
+++ CourseManagementService/Migrations/20241219105740_RemoveMaxLengthOfNoteContent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseManagementService.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMaxLengthOfNoteContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Notes",
+                type: "text",
+                nullable: true,
+                comment: "Note about the highlighted text",
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300,
+                oldNullable: true,
+                oldComment: "Note about the highlighted text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Notes",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true,
+                comment: "Note about the highlighted text",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true,
+                oldComment: "Note about the highlighted text");
+        }
+    }
+}
