8696e40efbce0cf943964c8bf4a2dc26b82287ce
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-12-23 18:16:49 +0700

    fix: [PBL6-294] Fix errors about the lesson tracking

---
+++ CourseManagementService/Migrations/20241223111454_AddIsUniqueInLessonTrackingTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseManagementService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsUniqueInLessonTrackingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LessonTrackings_LessonId_StudentId",
+                table: "LessonTrackings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LessonTrackings_LessonId_StudentId",
+                table: "LessonTrackings",
+                columns: new[] { "LessonId", "StudentId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LessonTrackings_LessonId_StudentId",
+                table: "LessonTrackings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LessonTrackings_LessonId_StudentId",
+                table: "LessonTrackings",
+                columns: new[] { "LessonId", "StudentId" });
+        }
+    }
+}
