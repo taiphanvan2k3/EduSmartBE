06c50b717e778566328757eb49602e96b6ac7067
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-12-23 11:30:08 +0700

    fix: [PBL6-290] Update CORS for Admin

--- PaymentService/Extensions/ServicesExtensions.cs
+++ PaymentService/Extensions/ServicesExtensions.cs
@@ -123,7 +123,8 @@ namespace PaymentService.Extensions
                 options.AddPolicy("AllowSpecificOrigin",
                     builder =>
                     {
-                        builder.WithOrigins("http://localhost:3000", "http://localhost:3030", "http://127.0.0.1:5500", "https://edu-smart-dut.vercel.app")
+                        builder.WithOrigins("http://localhost:3000", "http://localhost:3030", "http://127.0.0.1:5500",
+                                "https://edu-smart-dut.vercel.app", "https://edu-smart-admin.vercel.app")
                             .AllowAnyHeader()
                             .AllowAnyMethod()
                             .AllowCredentials(); // Cho phép client gửi cookie qua cross-origin
