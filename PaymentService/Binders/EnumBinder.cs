2d32ef6750774dd1fcc97f5f33f0343d48e964c3
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-12-23 10:38:56 +0700

    fix: [PBL6-283] Filter withdrawal request by status

---
+++ PaymentService/Binders/EnumBinder.cs
@@ -0,0 +1,45 @@
+using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace PaymentService.Binders
+{
+    public class EnumBinder<TEnum> : IModelBinder where TEnum : struct, Enum
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.FieldName).FirstValue;
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return Task.CompletedTask;
+            }
+
+            var enumValues = Enum.GetValues(typeof(TEnum)).Cast<Enum>();
+
+            foreach (var enumValue in enumValues)
+            {
+                var enumName = enumValue.GetType().GetField(enumValue.ToString()).Name;
+
+                // Nếu như dùng EnumMemberAttribute thì so sánh giá trị của EnumMemberAttribute với value
+                if (enumValue.GetType()
+                    .GetField(enumValue.ToString())
+                    .GetCustomAttributes(typeof(EnumMemberAttribute), false)
+                    .FirstOrDefault() is EnumMemberAttribute enumMember && enumMember.Value.Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    bindingContext.Result = ModelBindingResult.Success(enumValue);
+                    return Task.CompletedTask;
+                }
+
+                // Nếu không dùng EnumMemberAttribute thì so sánh tên enum với value
+                if (enumName.Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    bindingContext.Result = ModelBindingResult.Success(enumValue);
+                    return Task.CompletedTask;
+                }
+            }
+
+            bindingContext.Result = ModelBindingResult.Failed();
+            return Task.CompletedTask;
+        }
+    }
+}
