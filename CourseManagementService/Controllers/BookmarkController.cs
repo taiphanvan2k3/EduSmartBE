7e1691c8bab10b1b5e858d81862abd79a813a55a
Dinh Manh <tranmanhproh1@gmail.com>
2024-12-29 00:47:36 +0700

    feat: [PBL6-313] Get list bookmark lession ids by course id

--- CourseManagementService/Controllers/BookmarkController.cs
+++ CourseManagementService/Controllers/BookmarkController.cs
@@ -1,3 +1,4 @@
+using CourseManagementService.Common;
 using CourseManagementService.Services.BookmarkManagement;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,20 @@ namespace CourseManagementService.Controllers
         {
             var responseInfo = await _bookmarkService.ToggleBookmark(lessonId);
             return HandleResponseInfo(responseInfo, resourceName: "bookmarkStatus");
+        }
+
+        /// <summary>
+        /// Get list of bookmarked lesson ids by course id
+        /// <para>Created at: 2024/12/29</para>
+        /// <para>Created by: ManhTD</para>
+        /// </summary>
+        /// <param name="courseId"></param>
+        /// <returns></returns>
+        [HttpGet("courses/{courseId}/bookmarked-lessons")]
+        public async Task<IActionResult> GetBookmarkedLessonsByCourse([FromRoute] Guid courseId)
+        {
+            var bookmarkedLessons = await _bookmarkService.GetBookmarkedLessonIds(courseId);
+            return Ok(bookmarkedLessons);
         }
     }
 }
